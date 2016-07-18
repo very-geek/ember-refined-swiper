b937164d732ae83f68f74880710c87fa8956fbf9
nightire <nightire@very-geek.com>
2016-07-18 19:44:27 +0800

    🧀 Bring in Swiper from bower
    Signed-off-by: nightire <nightire@very-geek.com>

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-refined-swiper'
+  name: 'ember-refined-swiper',
+
+  included(app) {
+    this._super.included(app);
+
+    const swiperOptions = app.options['ember-refined-swiper'];
+
+    if (swiperOptions.vanilla) {
+      this.app.import(app.bowerDirectory + '/Swiper/dist/css/swiper.css');
+      this.app.import(app.bowerDirectory + '/Swiper/dist/js/swiper.min.js');
+    } else {
+      this.app.import(app.bowerDirectory + '/Swiper/dist/css/swiper.min.css');
+      this.app.import(app.bowerDirectory + '/Swiper/dist/js/swiper.jquery.umd.min.js');
+    }
+  }
 };
